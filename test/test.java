e1785571a3552e1d9066f106399a74717730da45
김충훈 <kchun0513@hanyang.ac.kr>
2020-10-24 21:11:14 +0900

    b

--- test/test.java
+++ test/test.java
@@ -3,16 +3,20 @@ import java.util.*;
 public class test {
 
 	public static void main(String[] args) {
-		int add, sub, mul;
-		double div;
+		double add, sub, mul, div;
 		Scanner sc = new Scanner(System.in);
 		double a = sc.nextDouble();
 		double b = sc.nextDouble();
 		
-		System.out.println("add : " + (a+b)
-				+ " sub : " + (a-b)
-				+ " mul : " + (a*b)
-				+ " div : " + (a/b));
+		add = a+b;
+		sub = a-b;
+		mul = a*b;
+		div = a/b;
+		
+		System.out.println("add : " + add
+				+ " sub : " + sub
+				+ " mul : " + mul
+				+ " div : " + div);
 		
 
 
